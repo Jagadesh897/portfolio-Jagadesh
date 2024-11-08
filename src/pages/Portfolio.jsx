10bfce48503a56c50ce1b4437a6d6372e5c100cc
Jagadesh897 <havocjaga756@email.com>
2024-11-08 20:27:05 +0530

    resume update

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -28,7 +28,7 @@ export default function Portfolio() {
 
   const handleDownloadResume = () => {
     // The PDF file should be placed in the public folder of your Next.js project
-    const resumeUrl = 'https://drive.google.com/drive/u/0/folders/1tGzh1pniOFHkhG9cxYDWCij3gz1HGpUe'
+    const resumeUrl = 'https://drive.google.com/file/d/1ftXFBmlD0FwbcYwG8-KUlswX--w9uR-7/view?usp=sharing'
     
     // Create a temporary anchor element
     const link = document.createElement('a')
